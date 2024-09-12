8bb69d3f852b1ce5618d6c4187a131d9dba52ac9
cc23140 <cc23140@g.unicamp.br>
2024-09-12 10:52:30 -0300

    Cadastro e Login funcionando!

--- android/app/src/main/kotlin/com/kuiz/kuiz/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.kuiz.kuiz
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity(){}
---
+++ kuiz_app/android/app/src/main/kotlin/com/kuiz/kuiz/kuiz_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kuiz.kuiz.kuiz_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
